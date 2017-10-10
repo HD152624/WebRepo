6579621fef3293cc344d582c8617edfd1f0caf8f
Seunghwa Lee <shle0509@gmail.com>
2017-10-10 12:24:30 +0900

    1

---
+++ WebClass/src/org/dimigo/servlet/BlogLoginServlet.java
@@ -0,0 +1,81 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.dimigo.vo.UserVO;
+import org.json.simple.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/bloglogin")
+public class BlogLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BlogLoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("myblog/login.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		boolean result = "test@naver.com".equals(id);
+		
+		if(result) {
+			HttpSession session = request.getSession();
+			UserVo user = new UserVo();
+			user.setId(id);
+			user.setPw(pw);
+			user.setName("È«±æµ¿");
+			user.setNick("ÀÇÀû");
+			session.setAttribute("user", user);
+			RequestDispatcher rd = request.getRequestDispatcher("myblog/home.jsp");
+			rd.forward(request, response);
+			
+		}
+		else {
+			HttpSession session = request.getSession();
+			UserVo user = new UserVo();
+			user.setId(id);
+			user.setPw(pw);
+			session.setAttribute("user", user);
+			request.setAttribute("msg", "error");
+			RequestDispatcher rd = request.getRequestDispatcher("myblog/login.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+}
--- WebClass/src/org/dimigo/servlet/LoginServlet.java
+++ WebClass/src/org/dimigo/servlet/LoginServlet.java
@@ -3,12 +3,15 @@ package org.dimigo.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.dimigo.vo.UserVO;
 import org.json.simple.JSONObject;
 
 import com.google.gson.Gson;
@@ -33,7 +36,8 @@ public class LoginServlet extends HttpServlet {
     */
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       // TODO Auto-generated method stub
-      response.getWriter().append("Served at: ").append(request.getContextPath());
+      RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp");
+      rd.forward(request,  response);
    }
 
    /**
@@ -48,27 +52,25 @@ public class LoginServlet extends HttpServlet {
       String pwd = request.getParameter("pwd");
       System.out.printf("id : %s, pwd : %s\n", id, pwd);
       
-      /*
-       *  {
-       *     "id" : "test"
-       *  }
-       */
+      boolean result = true;
       
-//      out.println("{");
-//      out.println("\"id\" : " + id);
-//      out.println("}");
-      
-      // JSON Simple library 사용
-//      JSONObject json = new JSONObject();
-//      json.put("id", id);
-//      System.out.println(json.toJSONString());
-//      out.write(json.toJSONString());
-      
-      Gson gson = new Gson();
-      gson.
-      
-      
+      if(result) {
+    	  // 세션에 사용자 정보 생성
+    	 HttpSession session = request.getSession();
+    	  
+    	 UserVO user = new UserVO();
+    	 user.setId(id);
+    	 user.setName("홍길동");
+    	 user.setNickname("의적");
+    	  
+    	 session.setAttribute("user", user);
+    	 RequestDispatcher rd = request.getRequestDispatcher("jsp/home.jsp");
+    	 rd.forward(request,  response);
+      } else {
+    	  request.setAttribute("msg", "error");
+    	  RequestDispatcher rd = request.getRequestDispatcher("jsp/login.jsp");
+    	  rd.forward(request,  response);
+      }
       out.close();
    }
-
 }
